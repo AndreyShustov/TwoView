60cc159ee0be9a7f3fc95af664ac2fc6dcaefcf7
Andrey Shustov <andrejshusto@mail.ru>
2021-08-18 11:56:42 +0300

    Initial Commit

---
+++ TwoView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoView
+//
+//  Created by Андрей  Шустов on 18.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
